b2d8b04daa08bcd5f4191eea0e3d39532a5814ab
John Nystrom <eta077@yahoo.com>
2023-05-04 15:31:49 -0500

    better click handling

--- src/kids/kids.mjs
+++ src/kids/kids.mjs
@@ -3,6 +3,29 @@ import htm from "https://unpkg.com/htm?module";
 
 const html = htm.bind(h);
 
+let selectedLesson = null;
+
+function onLessonClick(lessonInfo) {
+  let target = document.getElementById("container_" + lessonInfo.name);
+  if (selectedLesson) {
+    let lesson = selectedLesson.children.namedItem("lesson");
+    lesson.classList.remove("border-3");
+    lesson.classList.add("border-1");
+    let parts = selectedLesson.children.namedItem("parts");
+    parts.innerHTML = "";
+    if (selectedLesson.id == target.id) {
+      selectedLesson = null;
+      return;
+    }
+  }
+  selectedLesson = target;
+  let lesson = selectedLesson.children.namedItem("lesson");
+  lesson.classList.remove("border-1");
+  lesson.classList.add("border-3");
+  let parts = selectedLesson.children.namedItem("parts");
+  parts.innerHTML = LessonPartsNode(lessonInfo.available_parts);
+}
+
 function App(props) {
   return html`
     <div class="container-fluid">
@@ -16,10 +39,9 @@ function App(props) {
 function LessonNode(lesson) {
   return html`
     <div id="container_${lesson.name}" class="row">
-      <div id="parts_${lesson.name}" name="parts" class="col-3 invisible">
-        <${LessonPartsNode} parts=${lesson.available_parts}></${LessonPartsNode}>
+      <div id="parts_${lesson.name}" name="parts" class="col-3">
       </div>
-      <div id="lesson_${lesson.name}" name="lesson" class="col-9 border border-primary border-1">
+      <div id="lesson_${lesson.name}" name="lesson" class="col-9 border border-primary border-1" onClick="${(event) => onLessonClick(lesson)}">
         ${lesson.name}
       </div>
     </div>
@@ -28,7 +50,7 @@ function LessonNode(lesson) {
 
 function LessonPartsNode(parts) {
   return html`
-    ${parts.parts.map((part) => {
+    ${parts.map((part) => {
       return html`
         ${part.name}
       `;
@@ -36,7 +58,6 @@ function LessonPartsNode(parts) {
   `;
 }
 
-let selectedLesson = null;
 
 let response = await fetch("/kids/lessons");
 if (response.status == 200) {
@@ -44,27 +65,3 @@ if (response.status == 200) {
   render(html`<${App} lessons=${json}></${App}>`, document.body);
 }
 
-window.addEventListener("click", (event) => {
-  if (event.target.id && event.target.id.startsWith("lesson_")) {
-    let target = event.target.parentElement;
-    if (selectedLesson) {
-        let lesson = selectedLesson.children.namedItem("lesson");
-        lesson.classList.remove("border-3");
-        lesson.classList.add("border-1");
-        let parts = selectedLesson.children.namedItem("parts");
-        parts.classList.remove("visible");
-        parts.classList.add("invisible");
-      if (selectedLesson.id == target.id) {
-        selectedLesson = null;
-        return;
-      }
-    }
-    selectedLesson = target;
-    let lesson = selectedLesson.children.namedItem("lesson");
-    lesson.classList.remove("border-1");
-    lesson.classList.add("border-3");
-    let parts = selectedLesson.children.namedItem("parts");
-    parts.classList.remove("invisible");
-    parts.classList.add("visible");
-  }
-});
