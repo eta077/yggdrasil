ef4fe96cdbb134b40b631a66e318fde5f0306d1a
John Nystrom <eta077@yahoo.com>
2023-05-05 17:21:18 -0500

    added ability to drag back to available parts

--- src/kids/kids.mjs
+++ src/kids/kids.mjs
@@ -13,6 +13,8 @@ function onLessonClick(lessonInfo) {
     lesson.classList.remove("border-3");
     lesson.classList.add("border-1");
     let parts = selectedLesson.children.namedItem("parts");
+    parts.classList.remove("border");
+    parts.classList.remove("border-3");
     parts.innerHTML = "";
     if (selectedLesson.id == target.id) {
       selectedLesson = null;
@@ -24,6 +26,8 @@ function onLessonClick(lessonInfo) {
   lesson.classList.remove("border-1");
   lesson.classList.add("border-3");
   let parts = selectedLesson.children.namedItem("parts");
+  parts.classList.add("border");
+  parts.classList.add("border-3");
   render(html`<${LessonPartsNode} parts=${lessonInfo.available_parts}></${LessonPartsNode}`, parts);
 }
 
@@ -49,13 +53,15 @@ function onLessonDrop(event, lessonInfo) {
   console.log("onLessonDrop");
   event.preventDefault();
 
+  let changing_part = null;
   let avail_parts = lessonInfo.available_parts;
   for (var i = 0; i < avail_parts.length; i++) {
     if (avail_parts[i].name == draggedPart.innerHTML) {
-      avail_parts.splice(i, 1);
+      changing_part = avail_parts.splice(i, 1)[0];
       break;
     }
   }
+  lessonInfo.current_parts.push(changing_part);
 
   fetch("/kids/set-lesson", {
     method: "POST",
@@ -66,6 +72,39 @@ function onLessonDrop(event, lessonInfo) {
   });
 
   event.target.appendChild(draggedPart);
+}
+
+function onPartsDragOver(event) {
+  event.preventDefault();
+}
+
+function onPartsDragEnter(event) {
+  
+}
+
+function onPartsDrop(event, lessonInfo) {
+  event.preventDefault();
+
+  let changing_part = null;
+  let cur_parts = lessonInfo.current_parts;
+  for (var i = 0; i < cur_parts.length; i++) {
+    if (cur_parts[i].name == draggedPart.innerHTML) {
+      changing_part = cur_parts.splice(i, 1)[0];
+      break;
+    }
+  }
+  lessonInfo.available_parts.push(changing_part);
+
+  fetch("/kids/set-lesson", {
+    method: "POST",
+    body: JSON.stringify(lessonInfo),
+    headers: {
+      "Content-type": "application/json; charset=utf-8"
+    }
+  });
+
+  let target = document.getElementById("parts_" + lessonInfo.name);
+  target.appendChild(draggedPart);
 }
 
 function App(props) {
@@ -81,7 +120,7 @@ function App(props) {
 function LessonNode(lesson) {
   return html`
     <div id="container_${lesson.name}" class="row">
-      <div id="parts_${lesson.name}" name="parts" class="col-3">
+      <div id="parts_${lesson.name}" name="parts" class="col-3 py-3" onDragOver="${(event) => onPartsDragOver(event)}" onDragEnter="${(event) => onPartsDragEnter(event)}" onDrop="${(event) => onPartsDrop(event, lesson)}">
       </div>
       <div id="lesson_${lesson.name}" name="lesson" class="col-9 border border-primary border-1" onClick="${(event) => onLessonClick(lesson)}" onDragOver="${(event) => onLessonDragOver(event)}" onDragEnter="${(event) => onLessonDragEnter(event)}" onDrop="${(event) => onLessonDrop(event, lesson)}">
         ${lesson.name}
